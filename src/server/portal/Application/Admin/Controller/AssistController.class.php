7629245ce1539cccd9ff64b4af6c8b0bef619697
xuebingwang <406964108@qq.com>
2015-09-19 21:35:02 +0800

    还原误删除的文件 去掉无用的代码

---
+++ src/server/portal/Application/Admin/Controller/AssistController.class.php
@@ -0,0 +1,162 @@
+<?php
+// +----------------------------------------------------------------------
+// | OneThink [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 http://www.onethink.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: huajie <banhuajie@163.com>
+// +----------------------------------------------------------------------
+
+namespace Admin\Controller;
+
+/**
+ * 行为控制器
+ * @author huajie <banhuajie@163.com>
+ */
+class AssistController extends AdminController {
+
+    /**
+     * 行为日志列表
+     * @author huajie <banhuajie@163.com>
+     */
+	private $_size;
+
+	private $_uid;
+
+	public function _initialize(){
+		$this->_size = 30;
+		$this->_uid= session('user_auth')['uid'];
+		parent:: _initialize();
+	}
+	public function index(){
+		if(!empty(I('parent_name')))
+		{
+			$map['parent_name']= array('like', '%'.I('parent_name').'%');
+		}
+		if(!empty(I('baby_sex')))
+		{
+			$map['baby_sex']= I('baby_sex');
+		}
+		$map['status']    =   'CRT';
+		$map['current_assistant_id'] = $this->_uid;   
+		$list =M('khkf_candidate')->where($map)->limit($this->_size)->select();
+		$this->assign('_list', $list);
+		$this->meta_title = '待开发客户列表';
+		$this->display();
+	}
+
+	/**
+	 *编辑 
+	 */
+	public function show($id = 0){
+		empty($id) && $this->error('参数错误！');
+		$map = ['id'=>$id,'current_assistant_id'=>$this->_uid,'status'=>'CRT'];
+		$info = M('khkf_candidate')->field(true)->find($map);
+		empty($info) && $this->error('请求的数据不存在！');
+		$this->assign('item',$info);
+		$this->meta_title = '查看客户信息';
+		$this->display();
+	}
+    public function add(){
+        $this->meta_title = '添加客户信息';
+        $this->display();
+    }
+    public function save(){
+        $data['parent_name'] = I('post.parent_name');
+        empty($data['parent_name']) && $this->error('请输入家长姓名');
+        $data['mobile_num'] = I('post.mobile_num');
+        empty($data['mobile_num']) && $this->error('请输入家长电话号码');
+        $data['baby_name'] = I('post.baby_name');
+        empty($data['baby_name']) && $this->error('请输入宝贝姓名');
+        $data['baby_sex'] = I('post.baby_sex');
+        empty($data['baby_sex']) && $this->error('请输入宝贝性别');
+        $data['star'] = I('post.star');
+        $data['star'] =='' && $this->error('请输入候选人星数');
+        $data['district'] = I('post.district');
+        empty($data['district']) && $this->error('请输入家庭所在城市区域');
+        $data['city'] = I('post.city');
+        empty($data['city']) && $this->error('请输入家庭所有城市');
+        $data['baby_birthday']= I('post.baby_birthday');
+        empty($data['baby_birthday']) && $this->error('请输入宝宝出生年月');
+        $data['level']= I('post.level');
+        $id = I('post.id');
+	if(empty($id))
+	{
+		$data['insert_time'] = date("Y-m-d H:i:s");
+		$data['status'] = "CRT";
+		$res = M('khkf_candidate')->add($data);
+	}else{
+		$data['update_time'] = date("Y-m-d H:i:s");
+		$res = M('khkf_candidate')->where('id='.$id)->save($data);
+	} 
+        if($res !== false){
+            $this->success('保存成功！',U('custommanage/index'));
+        }else {
+            $this->error('保存失败！');
+        }
+    }
+
+    public function allocate(){
+	    if(!empty(I('parent_name')))
+	    {
+		    $map['parent_name']= array('like', '%'.I('parent_name').'%');
+	    }
+	    if(!empty(I('mobile_num')))
+	    {
+		    $map['mobile_num']= I('parent_name');
+	    }
+	    if(!empty(I('baby_name')))
+	    {
+		    $map['baby_name']= array('like', '%'.I('baby_name').'%');
+	    }
+	    $map['status']    =   'CRT';
+	    $list   =   $this->lists('khkf_candidate', $map);
+	    $this->assign('_list', $list);
+	    $this->meta_title = '调配客户列表';
+	    $this->display();
+    }
+    public function allocatesave()
+    {
+	    $current_assistant_id = I('post.current_assistant_id');
+	    $id = I('post.id');
+	    empty($id) && $this->error('参数错误');
+	    $model = M('khkf_candidate');
+	    $model->startTrans(); 
+	    $res = $model->where('id='.$id)->save(['current_assistant_id'=>$current_assistant_id,'update_time'=>date("Y-m-d H:i:s")]);
+	    if($res !== false){
+		    $rest =  M('khkf_candidate_develop')->add(['candidate_id'=>$id,'assistant_id'=>$current_assistant_id,'insert_time'=>date("Y-m-d H:i:s")]);
+		    if($rets!==false)
+		    {
+			    $model->commit();
+			    $this->success('保存成功！',U('custommanage/allocate'));
+		    }
+		    $model->rollback(); 
+		    $this->error('保存失败！');
+	    }else {
+		    $model->rollback(); 
+		    $this->error('保存失败！');
+	    }
+    }
+    public function allocateinfo($id=0)
+    {
+        empty($id) && $this->error('参数错误！');
+        $info = M('khkf_candidate')->field(true)->find($id);
+        $this->assign('item', $info);
+        $assi = M('ucenter_member')->field('id,username')->where("user_type='ASST'")->select();
+        $this->assign('assi', $assi);
+        $this->meta_title = '客户信息详情';
+        $this->display();
+    }
+    public function setStatus()
+    {
+	    $type = I('get.type');
+	    $id = I('get.id');
+	    $status = $type=='S'?'OK#':'FLS';
+	    $res = M('khkf_candidate')->where('id='.$id)->save(['update_time'=>date('Y-m-d H:i:s'),'status'=>$status]);
+	    if($res !== false){
+		    $this->success('设置完成！',U('custommanage/allocate'));
+	    }else {
+		    $this->error('保存失败！');
+	    }
+    }
+}
---
+++ src/server/portal/Application/Admin/View/ace/Knowledge/answer.php
@@ -0,0 +1,40 @@
+<extend name="Public/base" />
+
+<block name="body">
+    <form class="form-horizontal" method="post" action="/admin.php?s=/knowledge/edit/model/22.html" id="form">
+        <div class="tab-content no-border padding-24">
+            <!-- 基础文档模型 -->
+            <div class="tab-pane active tab1" id="tab1">
+                <div class="form-group cf">
+                    <label class="col-xs-12 col-sm-2 control-label no-padding-right">回复内容</label>
+                    <div class="col-xs-12 col-sm-6">
+                        <textarea class="width-100" name="answer"></textarea>
+                    </div>
+                    <div class="help-block col-xs-12 col-sm-reset inline">
+                    </div>
+                </div>
+            </div>
+            <div class="clearfix form-actions">
+                <div class="col-xs-12 center">
+                    <input type="hidden" value="1" name="id">
+                    <button id="sub-btn" class="btn btn-success ajax-post no-refresh" target-form="form-horizontal" type="submit">
+                        <i class="icon-ok bigger-110"></i> 确认保存
+                    </button>
+                    <button id="reset-btn" class="btn" type="reset">
+                        <i class="icon-undo bigger-110"></i> 重置
+                    </button>
+                    <a href="javascript:;" class="btn btn-info" onclick="history.go(-1)">
+                        <i class="icon-reply"></i>返回上一页
+                    </a>
+                </div>
+            </div>
+        </div>
+    </form>
+</block>
+
+<block name="script">
+    <script>
+        //导航高亮
+        highlight_subnav('{:U('knowledge/bbwdlist')}');
+    </script>
+</block>
