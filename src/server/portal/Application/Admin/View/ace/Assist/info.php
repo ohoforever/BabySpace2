54986566fbcae7ff710265debb0cbe1379ce6c98
wangyf <276180638@qq.com>
2015-09-21 15:35:58 +0800

    完成试听预约管理

--- src/server/portal/Application/Admin/View/ace/Assist/info.php
+++ src/server/portal/Application/Admin/View/ace/Assist/info.php
@@ -4,27 +4,27 @@
 	<!-- 表单 -->
 <?php $sex = ['MALE###'=>'男宝宝','FEMALE#'=>'女宝宝','UNKNOWN'=>'不详'];?>
 <div class="widget-box" style="opacity: 1; z-index: 0;margin-bottom:1em;">
-<div class="widget-header header-color-blue">
+<div class="widget-header " style="color:#999;">
           <h5 class="bigger lighter"> 客户信息</h5>        
 </div>
 <div class="widget-body">
 <div class=""> 
-       <table class="table table-striped table-bordered table-hover" style="margin-bottom:0px;">
+       <table class="table  table-bordered " style="margin-bottom:0px;">
 	<tbody>
 		<tr>
-			<td>家长名称:{$item.parent_name}</td>
-			<td>家长电话:{$item.mobile_num}</td>
+			<td><span style="color:#999;padding-right:8px;">家长名称:</span>{$item.parent_name}</td>
+			<td><span style="color:#999;padding-right:8px;">家长电话:</span>{$item.mobile_num}</td>
 		</tr>
 		<tr>
-			<td>宝宝名称:{$item.baby_name}</td>
-			<td>宝宝性别:<?php echo $sex[$item['baby_sex']];?></td>
+			<td><span style="color:#999;padding-right:8px;">宝宝名称:</span>{$item.baby_name}</td>
+			<td><span style="color:#999;padding-right:8px;">宝宝性别:</span><?php echo $sex[$item['baby_sex']];?></td>
 		</tr>
 		<tr>
-			<td>家庭所在城市:{$item.city}</td>
-			<td>家庭所在城市区域:{$item.district}</td>
+			<td><span style="color:#999;padding-right:8px;">家庭所在城市:</span>{$item.city}</td>
+			<td><span style="color:#999;padding-right:8px;">家庭所在城市区域:</span>{$item.district}</td>
 		</tr>
 		<tr>
-			<td>宝宝生日:{$item.baby_birthday}</td>
+			<td><span style="color:#999;padding-right:8px;">宝宝生日:</span>{$item.baby_birthday}</td>
 			<td></td>
 		</tr>
 	 </tbody>
@@ -34,7 +34,7 @@
 </div>
 <?php foreach($list as $v){?>
 <div class="widget-box" style="opacity: 1; z-index: 0;margin-bottom:1em;">
-<div class="widget-header header-color-blue">
+<div class="widget-header" style="color:#999;">
           <h5 class="bigger lighter">标注人:<?php echo $v['assistant_id'];?></h5>        
 </div>
 <div class="widget-body">
@@ -42,11 +42,11 @@
        <table class="table table-striped table-bordered table-hover" style="margin-bottom:0px;">
 	<tbody>
 		<tr>
-			<td>用户级别:<?php echo $v['level']?></td>
-			<td>候选人星数:<?php echo $v['star']?></td>
+			<td><span style="color:#999;padding-right:8px;">用户级别:</span><?php echo $v['level']?></td>
+			<td><span style="color:#999;padding-right:8px;">候选人星数:</span><?php echo $v['star']?></td>
 		</tr>
 		<tr>
-			<td colspan="2">内容:<?php echo $v['evaluation']?></td>
+			<td colspan="2"><span style="color:#999;padding-right:8px;">内容:</span><?php echo $v['evaluation']?></td>
 		</tr>
 	 </tbody>
 	</table>
@@ -55,9 +55,9 @@
 </div>
 <?php }?>
 
-	<form id="form" action="{:U('allocatesave')}" method="post" class="form-horizontal">
+	<form id="form" action="{:U('save')}" method="post" class="form-horizontal">
 <div class="widget-box" style="opacity: 1; z-index: 0;margin-bottom:1em;">
-<div class="widget-header header-color-blue">
+<div class="widget-header " style="color:#999;">
           <h5 class="bigger lighter"> 添加评级</h5>        
 </div>
 <div class="widget-body">
@@ -66,14 +66,14 @@
 	<tbody>
 		<tr>
 			<td><span>评级:</span>
-				<select name="current_assistant_id">
+				<select name="level">
 				<option value="A" >A级</option>
 				<option value="B" >B级</option>
 				<option value="C" >C级</option>
 				</select>
 			</td>
 			<td><span>候选人星数:</span>
-				<select name="current_assistant_id">
+				<select name="star">
 				<?php for ($i=1;$i<=5;$i++){?>
 				<option value="<?php echo $i?>" ><?php echo $i?></option>
 				<?php }?>
@@ -82,8 +82,7 @@
 		</tr>
 		<tr>
 			<td colspan="2">内容:
-			<textarea name="evaluation" rows="10" cols="15" style="width:100%;">
-			</textarea>
+			<textarea name="evaluation" rows="10" cols="15" style="width:100%;"></textarea>
 			</td>
 		</tr>
 	 </tbody>
@@ -110,7 +109,7 @@
 <script type="text/javascript" charset="utf-8">
 	Think.setValue('type',{$type|default=1});
     //导航高亮
-    highlight_subnav('{:U('custommanage/allocate')}');
+    highlight_subnav('{:U('assist/index')}');
 </script>
     <script src="__ACE__/js/date-time/bootstrap-datepicker.min.js"></script>
     <script type="text/javascript">
