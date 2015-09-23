8b68a59e1af41b9e611901ad4508b724e0b2cad5
liudou <1248776541@qq.com>
2015-09-23 09:55:51 +0800

    报课管理部分功能

--- src/server/portal/Application/Admin/View/ace/Addcourse/add.php
+++ src/server/portal/Application/Admin/View/ace/Addcourse/add.php
@@ -13,50 +13,258 @@
 </div>
 <!-- 标签页导航 -->
 <div class="tabbable">
-    <ul class="nav nav-tabs padding-18">
-        
-        <volist name=":parse_config_attr($model['field_group'])" id="group">
-        <li <eq name="key" value="1">class="active"</eq>><a data-toggle="tab" href="#tab{$key}">{$group}</a></li>
-        </volist>
-    </ul>
+
     <!-- 表单 -->
     <form id="form" action="{:U('add?model='.$model['id'])}" method="post" class="form-horizontal">
     <div class="tab-content no-border padding-24">
         <!-- 基础文档模型 -->
-        <div class="widget-box">
-            <div class="widget-header">
-                <h5>Default Widget Box</h5>
 
-                <div class="widget-toolbar">
-                    <a data-action="settings" href="#">
-                        <i class="icon-cog"></i>
-                    </a>
+        <div class="col-xs-12 col-sm-6 widget-container-span ui-sortable" >
+            <div class="widget-box">
+                <div class="widget-header">
+                    <h5>用户信息</h5>
 
-                    <a data-action="reload" href="#">
-                        <i class="icon-refresh"></i>
-                    </a>
+                    <div class="widget-toolbar">
+                        <a data-action="settings" href="#">
+                            <i class="icon-cog"></i>
+                        </a>
 
-                    <a data-action="collapse" href="#">
-                        <i class="icon-chevron-up"></i>
-                    </a>
+                        <a data-action="reload" href="#">
+                            <i class="icon-refresh"></i>
+                        </a>
 
-                    <a data-action="close" href="#">
-                        <i class="icon-remove"></i>
-                    </a>
+                        <a data-action="collapse" href="#">
+                            <i class="icon-chevron-up"></i>
+                        </a>
+
+                        <a data-action="close" href="#">
+                            <i class="icon-remove"></i>
+                        </a>
+                    </div>
                 </div>
-            </div>
 
-            <div class="widget-body">
-                <div class="widget-main">
-                    <p class="alert alert-info">
-                        Nunc aliquam enim ut arcu aliquet adipiscing. Fusce dignissim volutpat justo non consectetur. Nulla fringilla eleifend consectetur.
-                    </p>
-                    <p class="alert alert-success">
-                        Raw denim you probably haven't heard of them jean shorts Austin.
-                    </p>
+                <div class="widget-body">
+                    <div class="widget-main">
+                        <div class="alert alert-info">
+
+                            <div class="row">
+                                <div class="col-xs-4">
+                                    <label style="padding-top: 4px;">用户姓名：</label>
+                                </div>
+                                <div class="col-xs-8">
+                                    <input type="text" name="user_name" id="user_name" value="" class="width-100" />
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-4">
+                                    <label style="padding-top: 4px;">联系号码：</label>
+                                </div>
+                                <div class="col-xs-8">
+                                    <input type="text" id="mobile_num"  name="mobile_num" value="" class="width-100" />
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-4">
+                                    <label style="padding-top: 4px;">所在城市：</label>
+                                </div>
+                                <div class="col-xs-8">
+                                    <input type="text" name="city" id="city" value="" class="width-100" />
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-4">
+                                    <label style="padding-top: 4px;">所在地区：</label>
+                                </div>
+                                <div class="col-xs-8">
+                                    <input type="text" name="district" id="district" value="" class="width-100" />
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-4">
+                                    <label style="padding-top: 4px;">QQ：</label>
+                                </div>
+                                <div class="col-xs-8">
+                                    <input type="text" name="qq" value="" id="qq" class="width-100" />
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-4">
+                                    <label style="padding-top: 4px;">Email：</label>
+                                </div>
+                                <div class="col-xs-8">
+                                    <input type="text" name="email" id="email" value="" class="width-100" />
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
+
+        <div class="col-xs-12 col-sm-6 widget-container-span ui-sortable" >
+            <div class="widget-box">
+                <div class="widget-header">
+                    <h5>宝贝信息</h5>
+
+                    <div class="widget-toolbar">
+                        <a data-action="settings" href="#">
+                            <i class="icon-cog"></i>
+                        </a>
+
+                        <a data-action="reload" href="#">
+                            <i class="icon-refresh"></i>
+                        </a>
+
+                        <a data-action="collapse" href="#">
+                            <i class="icon-chevron-up"></i>
+                        </a>
+
+                        <a data-action="close" href="#">
+                            <i class="icon-remove"></i>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="widget-body">
+                    <div class="widget-main">
+                        <div class="alert alert-success">
+
+
+                            <div class="row">
+                                <div class="col-xs-4">
+                                    <label style="padding-top: 4px;">宝贝姓名：</label>
+                                </div>
+                                <div class="col-xs-8" id="selectBaby">
+
+                                   <input type="text" name="baby_name" value="" class="width-100" />
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-4">
+                                    <label style="padding-top: 4px;">宝贝性别：</label>
+                                </div>
+                                <div class="col-xs-8">
+                                    <label>
+                                        <input type="radio" checked="checked" name="baby_sex" value="MALE###" class="ace">
+                                        <span class="lbl">&nbsp;&nbsp;&nbsp;&nbsp;小王子&nbsp;</span>
+                                    </label>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                    <label>
+                                        <input type="radio" name="baby_sex" value="FEMALE#" class="ace">
+                                        <span class="lbl">&nbsp;&nbsp;&nbsp;&nbsp;小公主&nbsp;</span>
+                                    </label>
+
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-4">
+                                    <label style="padding-top: 4px;">宝宝生日：</label>
+                                </div>
+                                <div class="col-xs-8">
+                                    <input class="width-100 time" id="baby_birthday" type="text" placeholder="请选择时间" value="" name="baby_birthday">
+
+                                </div>
+                            </div>
+                            <div class="row" style="margin-bottom: 90px;"></div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <br />
+        </div>
+
+        <div class="col-xs-12 col-sm-6 widget-container-span ui-sortable" >
+            <div class="widget-box">
+                <div class="widget-header">
+                    <h5>课程信息</h5>
+
+                    <div class="widget-toolbar">
+                        <a data-action="settings" href="#">
+                            <i class="icon-cog"></i>
+                        </a>
+
+                        <a data-action="reload" href="#">
+                            <i class="icon-refresh"></i>
+                        </a>
+
+                        <a data-action="collapse" href="#">
+                            <i class="icon-chevron-up"></i>
+                        </a>
+
+                        <a data-action="close" href="#">
+                            <i class="icon-remove"></i>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="widget-body">
+                    <div class="widget-main">
+                        <div class="alert alert-success">
+
+
+                            <div class="row">
+                                <div class="col-xs-4">
+                                    <label style="padding-top: 4px;">学校名称：</label>
+                                </div>
+                                <div class="col-xs-8">
+                                    <input type="text" name="school_name" id="school_name" value="" class="width-100" />
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-4">
+                                    <label style="padding-top: 4px;">课程名称：</label>
+                                </div>
+                                <div class="col-xs-8">
+                                    <input type="text" name="course_name" id="course_name" value="" class="width-100" />
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-4">
+                                    <label style="padding-top: 4px;">课程课时：</label>
+                                </div>
+                                <div class="col-xs-8">
+                                    <input type="text" name="course_total" id="course_total" value="" class="width-100" />
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-4">
+                                    <label style="padding-top: 4px;">买课费用(元)：</label>
+                                </div>
+                                <div class="col-xs-8">
+                                    <input type="text" name="course_amount" value="" id="course_amount" class="width-100" />
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-4">
+                                    <label style="padding-top: 4px;">购买课时：</label>
+                                </div>
+                                <div class="col-xs-8">
+                                    <input type="text" name="course_count" value="" id="course_count" class="width-100" />
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-4">
+                                    <label style="padding-top: 4px;">赠送课时：</label>
+                                </div>
+                                <div class="col-xs-8">
+                                    <input type="text" name="given_count" value="" id="given_count" class="width-100" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <br />
+        </div>
+
 
         <div class="clearfix form-actions">
             <div class="col-xs-12 center">
@@ -92,12 +300,58 @@ $(function(){
         minView:2,
         autoclose:true
     });
-    <?php if(isset($active_menu)):?>
-    //导航高亮
-    highlight_subnav('<?=U($active_menu)?>');
-    <?php else:?>
-    highlight_subnav('{:U('Model/index')}');
-    <?php endif;?>
+
+    highlight_subnav('{:U('Addcourse/add')}');
+
+    $('#mobile_num').keyup(function(event){
+        if(event.keyCode===13){
+            var data=$("#mobile_num").val();
+            $.post('<?=U('addcourse/query')?>',{data:data},function (data2) {
+                    $("#user_name").val(data2.userInfo.user_name);
+                    $("#mobile_num").val(data2.userInfo.mobile_num);
+                    $("#city").val(data2.userInfo.city);
+                    $("#district").val(data2.userInfo.district);
+                    $("#qq").val(data2.userInfo.qq);
+                    $("#email").val(data2.userInfo.email);
+                   if(data2.childInfo.length>0){
+
+                       var _html= '<select name="childId" id="childId"><option value="">--请选择--</option>';
+                       var childInfo = [];
+                       $.each(data2.childInfo,function(key,child){
+                            childInfo[child.id] = child;
+                           _html+='<option value="'+child.id+'">'+child.baby_name+'</option>';
+                       })
+
+                       _html+='</select>';
+                       //console.info(childInfo);
+                       $("#selectBaby").html(_html);
+                       $('#childId').change(function(){
+                           var obj = childInfo[this.value];
+                           $("#baby_birthday").val(obj.baby_birthday);
+                           $("input:radio[name='baby_sex']").each(function(){
+                               console.info(this.value);
+                               console.info(obj.baby_sex);
+                               console.info(this.value == obj.baby_sex);
+                               if(this.value == obj.baby_sex){
+                                   $(this).prop('checked',true);
+                               }
+                           })
+//                           if(data2.childInfo[this.value].baby_sex=="MALE###"){
+//                               $("input:radio[name='baby_sex'][data-par='MALE###']").attr("checked",true);
+//                               $("input:radio[name='baby_sex'][data-par='FEMALE#']").attr("checked",false);
+//
+//
+//                           }
+                          // console.info(obj);
+                       });
+
+                   }
+
+                },
+                'json');
+        }
+    });
+
 });
 </script>
 </block>
