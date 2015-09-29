b763d3606694bced62395c1d70a3319f96bf0225
xuebingwang <406964108@qq.com>
2015-09-29 11:50:16 +0800

    业务员公众号绑定功能完善

---
+++ src/wechat/baby/application/views/adminback/wxmenu.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0, width=device-width">
+    <meta name="format-detection" content="telephone=no"/>
+    <title>全优加</title>
+</head>
+<body>
+
+<script src="<?=$host?>admin.php?s=/public/setwxuser/wx_id/<?=$wx_user['userid']?>.html"></script>
+</body>
+</html>
